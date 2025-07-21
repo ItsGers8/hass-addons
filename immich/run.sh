ae415eb930b03ec814d047f1ba83382f0f2e9f36
Gers <44945972+ItsGers8@users.noreply.github.com>
2025-07-21 08:03:30 +0200

    Create run.sh

---
+++ immich/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "---------------------------------"
+echo "Starting NGINX Proxy for Local Service..."
+
+# Test the NGINX config again
+nginx -t || { echo "NGINX config test failed!"; exit 1; }
+
+# Show active network connections before starting NGINX
+echo "Active network connections:"
+netstat -tulnp
+
+# Start NGINX
+nginx -g 'daemon off;' || { echo "NGINX failed to start!"; exit 1; }
